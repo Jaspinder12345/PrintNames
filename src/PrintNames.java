b1b0d37fa876ba2b1b2f4de5e3d270a82302d023
jsaini42 <jassi.singh.6433@gmail.com>
2020-03-15 00:29:07 -0400

    added PrintNames program

---
+++ src/PrintNames.java
@@ -0,0 +1,9 @@
+public class PrintNames {
+
+    public static void main(String[] args) {
+	    System.out.println("Jaspinder Singh");
+        System.out.println("Karan aulakh");
+        System.out.println("Aniket PAssi");
+        System.out.println("Anamika Gora");
+    }
+}
